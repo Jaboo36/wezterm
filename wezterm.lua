66c9fe844482ff5846a61b5a8c7301434f67acd5
Josh Waterhouse <josh@waterhouseserver.home>
2025-06-12 23:09:48 -0700

    initial commit

---
+++ wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+-- Appearance
+config.color_scheme = "Monokai Remastered"
+
+config.font_size = 19
+
+config.default_cursor_style = "BlinkingBar"
+config.cursor_blink_rate = 600
+config.cursor_blink_ease_in = "Constant"
+config.cursor_blink_ease_out = "Constant"
