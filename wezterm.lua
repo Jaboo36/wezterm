54dc4d5ca130c81a6906e814f93ed61f7eceba23
Josh Waterhouse <56381148+Jaboo36@users.noreply.github.com>
2025-06-23 13:59:08 -0700

    update color scheme and quotation marks

--- wezterm.lua
+++ wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- Appearance
-config.color_scheme = "Monokai Remastered"
+config.color_scheme = "Kanagawa (Gogh)"
 
 config.font_size = 19
 
@@ -10,3 +10,5 @@ config.default_cursor_style = "BlinkingBar"
 config.cursor_blink_rate = 600
 config.cursor_blink_ease_in = "Constant"
 config.cursor_blink_ease_out = "Constant"
+
+return config
